5126f415e746d0f4e2270d8e0a92f77f55d8360b
DrBrooklyn <waternonne@gmail.com>
2019-08-28 01:05:36 +0200

    added samples as a bench for framing

---
+++ samples/yae.Framing.Client/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using Pipelines.Sockets.Unofficial;
+using yae.Framing.Sample.BasicFrame;
+
+namespace yae.Framing.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                //await socket.ConnectAsync("127.0.0.1", 5000);
+                await Task.Delay(2000);
+                var conn = await SocketConnection.ConnectAsync(new IPEndPoint(IPAddress.Loopback, 5000));
+                var producer = conn.Output.AsPipeFrameProducer(new HeaderBasicFrameEncoder());
+                await producer.ProduceAsync(ProduceFrames());
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine(ex);
+            }
+
+            Console.ReadLine();
+            Console.ReadLine();
+        }
+
+        public static int _messageId = 0;
+        private static Random rdm = new Random();
+
+        public static async IAsyncEnumerable<OutputFrame<BasicFrame>> ProduceFrames()
+        {
+            while (true)
+            {
+                var size = rdm.Next(0, ushort.MaxValue);
+
+                var frame = new BasicFrame {MessageId = _messageId++};
+                yield return new OutputFrame<BasicFrame>(frame, new byte[size]);
+                await Task.Delay(0);
+                Console.WriteLine($"Sent {size} bytes");
+            }
+        }
+    }
+}
---
+++ samples/yae.Framing.Server/Program.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Connections;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using yae.Framing.Sample.BasicFrame;
+
+namespace yae.Framing.Server
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            await CreateWebHostBuilder(args).Build().RunAsync();
+        }
+
+        public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+        {
+            return WebHost.CreateDefaultBuilder(args)
+                .UseKestrel(options =>
+                {
+                    options.ListenAnyIP(5000, builder => { builder.UseConnectionHandler<MyConnectionHandler>(); });
+                })
+                .UseStartup<Startup>();
+        }
+    }
+
+    class Startup : IDisposable
+    {
+        public void Dispose()
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IApplicationLifetime lifetime)
+        {
+            
+            /*if (env.IsDevelopment()) app.UseDeveloperExceptionPage();
+            app.Run(context => context.Response.WriteAsync($"clients: {_server.ClientCount}"));*/
+        }
+    }
+    class MyConnectionHandler : ConnectionHandler
+    {
+        private ILogger _logger;
+
+        public MyConnectionHandler(ILogger<MyConnectionHandler> logger) => _logger = logger;
+        public override async Task OnConnectedAsync(ConnectionContext connection)
+        {
+            try
+            {
+                _logger.LogInformation("Client connected");
+                var consumer = connection.Transport.Input.AsPipeFrameConsumer(new HeaderBasicFrameDecoder());
+                ulong total = 0;
+                var sw = Stopwatch.StartNew();
+                await foreach (var frame in consumer.ConsumeAsync())
+                {
+                    total += (ulong)frame.Payload.Length;
+                    _logger.LogInformation($"Received frame with MessageId={frame.Frame.MessageId}, Length={frame.Payload.Length}");
+                }
+                sw.Stop();
+                _logger.LogInformation($"Client sent {total} bytes of data in {sw.ElapsedMilliseconds} ms");
+                _logger.LogInformation("Client disconnected");
+            }
+            catch (Exception ex)
+            {
+                _logger.LogCritical(ex.ToString());
+            }
+        }
+    }
+}
